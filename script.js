93638e2e3301d071363fa28af4ae24256415fe29
rayantlbn <rayantlbn@gmail.com>
2023-12-08 00:08:46 +0100

    TabloRetro

---
+++ script.js
@@ -0,0 +1,10 @@
+/*document.querySelector('.retro70').addEventListener('click', function() {
+    document.body.style.background = '#ff007f';
+    document.body.style.color = '#fff';
+});
+
+document.querySelector('.chalkboard').addEventListener('click', function() {
+    document.body.style.background = '#333';
+    document.body.style.color = '#fff';
+});
+*/
